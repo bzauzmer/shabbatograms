05b18e0e54a265145c99479e1b4343c2f56176c6
bzauzmer <bzauzmer@gmail.com>
2020-05-30 11:43:11 -0700

    Update form_submit.py

--- form_submit.py
+++ form_submit.py
@@ -1,6 +1,6 @@
 from flask import request, redirect
 
-@app.route('/form_submit.py', methods = ['POST'])
+@app.route('form_submit.py', methods = ['POST'])
 def signup():
     your_name = request.form['your-name']
     print("Your name is '" + your_name + "'")
