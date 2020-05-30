03eb7ff6393b18b6a1a6a7a814d1d0ca9776b647
bzauzmer <bzauzmer@gmail.com>
2020-05-30 11:37:44 -0700

    Create form_submit.py

---
+++ form_submit.py
@@ -0,0 +1,7 @@
+from flask import request, redirect
+
+@app.route('/form_submit.py', methods = ['POST'])
+def signup():
+    your_name = request.form['your-name']
+    print("Your name is '" + your_name + "'")
+    return redirect('/')
