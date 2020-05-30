5c70672ccae973ce51555d438deb793cd25801f1
bzauzmer <bzauzmer@gmail.com>
2020-05-30 15:33:59 -0700

    Delete form_submit.py

--- form_submit.py
+++
@@ -1,7 +0,0 @@
-from flask import request, redirect
-
-@app.route('form_submit.py', methods = ['POST'])
-def signup():
-    your_name = request.form['your-name']
-    print("Your name is '" + your_name + "'")
-    return redirect('/')
