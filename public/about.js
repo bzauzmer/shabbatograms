9958bcec21816c9d65d09e04a3ad1706a39cf593
DODGERS_DOMAIN\BZauzmer <bzauzmer@ladodgers.com>
2020-06-15 00:09:10 -0700

    About Us

---
+++ public/about.js
@@ -0,0 +1,26 @@
+// Initialize Firebase app
+firebase.initializeApp(config);
+
+// Reference to the shabbatograms object in Firebase database
+var contacts = firebase.database().ref("contacts");
+
+// Save a new contact submission to the database, using the input in the form
+var contactForm = function () {
+
+  // Get input values from each of the form elements
+  var contact_name = $("#contact-name").val();
+  var contact_email = $("#contact-email").val();
+  var contact_message = $("#contact-message").val();
+
+  // Push a new form to the database using those values
+  contacts.push().set({
+    contact_name: contact_name,
+    contact_email: contact_email,
+    contact_message: contact_message
+  });
+
+  // Show post-submit message
+  $("#contact-form").hide();
+  $("#success-message").show();
+};
+
